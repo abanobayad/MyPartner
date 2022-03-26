db77dc88483eb6b6f3d700537ba7762aef36a45c
Abanob Ayad <76524886+abanobayad@users.noreply.github.com>
2022-03-26 22:14:10 +0200

    Solve Icons Error

--- Laravel/config/app.php
+++ Laravel/config/app.php
@@ -54,7 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('ASSET_URL', 'http://192.168.1.5/mypartner/laravel/public'),
+    'asset_url' => env('ASSET_URL', null),
 
     /*
     |--------------------------------------------------------------------------
--- Laravel/resources/views/Admin/inc/header.blade.php
+++ Laravel/resources/views/Admin/inc/header.blade.php
@@ -50,7 +50,7 @@
                 <div>
                     <a class="navbar-brand brand-logo"  href="{{ route('admin.home') }}">
                         {{-- <img src="{{ asset('images') }}/logo.svg" alt="logo" /> --}}
-                        <i class="mdi mdi-account-search"><span class="text-dark">My</span>Partner00</i>
+                        <i class="mdi mdi-account-search"><span class="text-dark">My</span>Partner</i>
                     </a>
                     <a class="navbar-brand brand-logo-mini" href="{{route('admin.home')}}">
                         {{-- <i>MyPartner</i> --}}
