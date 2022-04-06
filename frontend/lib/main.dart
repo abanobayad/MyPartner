0d27ca12d76cdcf7a79ebf53fc1d2ded8b2acbcc
Abanob Ayad <76524886+abanobayad@users.noreply.github.com>
2022-04-06 13:27:37 +0200

    Merge branch 'Frontend' into Backend

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/screens/signup.dart';
+import 'package:my_partner/login.dart';
+import 'package:my_partner/signup.dart';
 
-void main() => runApp(const MyApp());
+import 'homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyPartner',
-      theme: ThemeData(
-        colorScheme: Theme.of(context).colorScheme.copyWith(
-              primary: const Color.fromARGB(255, 193, 74, 230),
-            ),
-      ),
-      home: const SignUp(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignupPage(),
     );
   }
 }
